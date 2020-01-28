8b5e952d30adf26800415d5d0783e82b98b46a99
Igor <GI_Something@protonmail.com>
2020-01-28 18:54:26 +0200

    feat: Add possibility to save text to a file
    Added JButton "Save", and JTextField (filepath).

--- src/editor/TextEditor.java
+++ src/editor/TextEditor.java
@@ -2,8 +2,16 @@ package editor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class TextEditor extends JFrame {
+    JTextArea textAreaEditor;
+    JPanel northPanel;
+    JTextField textFieldFilepath;
+    JButton buttonSave;
+    JScrollPane scrollPaneEditor;
 
     public TextEditor() {
         createView();
@@ -16,14 +24,43 @@ public class TextEditor extends JFrame {
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
 
-        JTextArea jTextArea = new JTextArea();
-        jTextArea.setName("TextArea");
+        createMainField();
+        createNorthPanel();
 
-        JScrollPane jScrollPane = new JScrollPane(jTextArea);
-        jScrollPane.setName("ScrollPane");
-
-        add(jScrollPane);
+        add(scrollPaneEditor, BorderLayout.CENTER);
+        add(northPanel, BorderLayout.NORTH);
 
         setVisible(true);
+    }
+
+    private void createMainField() {
+        textAreaEditor = new JTextArea();
+        textAreaEditor.setName("TextArea");
+
+        scrollPaneEditor = new JScrollPane(textAreaEditor);
+        scrollPaneEditor.setName("ScrollPane");
+    }
+
+    private void createNorthPanel() {
+        northPanel = new JPanel();
+        northPanel.setLayout(new BoxLayout(northPanel, BoxLayout.X_AXIS));
+
+        textFieldFilepath = new JTextField();
+        textFieldFilepath.setName("FilenameField");
+
+        buttonSave = new JButton("Save");
+        buttonSave.setName("SaveButton");
+        buttonSave.addActionListener(actionEvent -> {
+            try {
+                Path filepath = Path.of(textFieldFilepath.getText().trim());
+                Files.writeString(filepath, textAreaEditor.getText());
+                JOptionPane.showMessageDialog(getContentPane(), "Saved", "Information", JOptionPane.INFORMATION_MESSAGE);
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(getContentPane(), "Something went wrong!", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        });
+
+        northPanel.add(textFieldFilepath);
+        northPanel.add(buttonSave);
     }
 }

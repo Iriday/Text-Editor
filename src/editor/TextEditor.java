6152ab0ffa2a56ede80084575e4f7dc642792e3d
Igor <GI_Something@protonmail.com>
2020-01-31 20:41:23 +0200

    feat: Add possibility to search for a string in text
    Added new buttons: startSearch/previousMatch/nextMatch.

--- src/editor/TextEditor.java
+++ src/editor/TextEditor.java
@@ -12,15 +12,22 @@ import java.nio.file.Path;
 public class TextEditor extends JFrame {
     private JTextArea textAreaEditor;
     private JPanel northPanel;
-    private JTextField textFieldFilepath;
+    private JTextField textFieldFilepathAndSearch;
     private JButton buttonSave;
     private JButton buttonOpen;
+    private JButton buttonNextMatch;
+    private JButton buttonPreviousMatch;
+    private JButton buttonStartSearch;
     private JScrollPane scrollPaneEditor;
     private JMenuBar menuBar;
 
+    private int searchStartPos = -1;
+    private int searchEndPos = -1;
+    private String searchText = "";
+
     private final ActionListener actionListenerSave = actionEvent -> {
         try {
-            Path filepath = Path.of(textFieldFilepath.getText().trim());
+            Path filepath = Path.of(textFieldFilepathAndSearch.getText().trim());
             Files.writeString(filepath, textAreaEditor.getText());
             JOptionPane.showMessageDialog(getContentPane(), "Saved", "Information", JOptionPane.INFORMATION_MESSAGE);
         } catch (IOException e) {
@@ -30,10 +37,61 @@ public class TextEditor extends JFrame {
 
     private final ActionListener actionListenerLoad = actionEvent -> {
         try {
-            Path filepath = Path.of(textFieldFilepath.getText());
+            Path filepath = Path.of(textFieldFilepathAndSearch.getText());
             textAreaEditor.setText(Files.readString(filepath));
         } catch (IOException e) {
             JOptionPane.showMessageDialog(getContentPane(), "Something went wrong!", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    };
+
+    private final ActionListener actionListenerStartSearch = actionEvent -> {
+        String text = textAreaEditor.getText();
+        searchText = textFieldFilepathAndSearch.getText();
+        searchStartPos = text.indexOf(searchText);
+
+        if (searchStartPos != -1) {
+            searchEndPos = searchStartPos + searchText.length();
+            textAreaEditor.setCaretPosition(searchEndPos);
+            textAreaEditor.select(searchStartPos, searchEndPos);
+            textAreaEditor.grabFocus();
+        }
+    };
+
+    private final ActionListener actionListenerPreviousMatch = actionEvent -> {
+        if (searchText.isEmpty()) {
+            return;
+        }
+        String text = textAreaEditor.getText().substring(0, searchEndPos - 1);
+        searchStartPos = text.lastIndexOf(searchText);
+
+        if (searchStartPos == -1) { //if the beginning start from the end
+            searchStartPos = textAreaEditor.getText().lastIndexOf(searchText);
+        }
+        if (searchStartPos != -1) {
+            searchEndPos = searchStartPos + searchText.length();
+
+            textAreaEditor.setCaretPosition(searchEndPos);
+            textAreaEditor.select(searchStartPos, searchEndPos);
+            textAreaEditor.grabFocus();
+        }
+    };
+
+    private final ActionListener actionListenerNextMatch = actionEvent -> {
+        if (searchText.isEmpty()) {
+            return;
+        }
+        String text = textAreaEditor.getText();
+        searchStartPos = text.indexOf(searchText, searchStartPos + 1/*end*/);
+
+        if (searchStartPos == -1) { //if the end start from the beginning
+            searchStartPos = text.indexOf(searchText);
+        }
+        if (searchStartPos != -1) {
+            searchEndPos = searchStartPos + searchText.length();
+
+            textAreaEditor.setCaretPosition(searchEndPos);
+            textAreaEditor.select(searchStartPos, searchEndPos);
+            textAreaEditor.grabFocus();
         }
     };
 
@@ -97,28 +155,55 @@ public class TextEditor extends JFrame {
     }
 
     private void createNorthPanel() {
+        int buttonWidth = 28;
+        int buttonHeight = 24;
+        int widthBetweenComponents = 2;
+
         northPanel = new JPanel();
         northPanel.setLayout(new BoxLayout(northPanel, BoxLayout.X_AXIS));
         northPanel.setBorder(new EmptyBorder(3, 8, 0, 8));
 
-        textFieldFilepath = new JTextField();
-        textFieldFilepath.setName("FilenameField");
-
-        buttonSave = new JButton(new ImageIcon("\\icons\\save.png"));
-        buttonSave.setName("SaveButton");
-        buttonSave.addActionListener(actionListenerSave);
-        setFixedSize(buttonSave, 28, 24);
 
         buttonOpen = new JButton(new ImageIcon("\\icons\\file.png"));
         buttonOpen.setName("OpenButton");
         buttonOpen.addActionListener(actionListenerLoad);
-        setFixedSize(buttonOpen, 28, 24);
+        setFixedSize(buttonOpen, buttonWidth, buttonHeight);
 
-        northPanel.add(textFieldFilepath);
-        northPanel.add(Box.createHorizontalStrut(2));
+        buttonSave = new JButton(new ImageIcon("\\icons\\save.png"));
+        buttonSave.setName("SaveButton");
+        buttonSave.addActionListener(actionListenerSave);
+        setFixedSize(buttonSave, buttonWidth, buttonHeight);
+
+        textFieldFilepathAndSearch = new JTextField();
+        textFieldFilepathAndSearch.setName("SearchField");
+
+        buttonStartSearch = new JButton(new ImageIcon("\\icons\\search.png"));
+        buttonStartSearch.setName("StartSearchButton");
+        setFixedSize(buttonStartSearch, buttonWidth, buttonHeight);
+        buttonStartSearch.addActionListener(actionListenerStartSearch);
+
+        buttonPreviousMatch = new JButton(new ImageIcon("\\icons\\arrowLeft.png"));
+        buttonPreviousMatch.setName("PreviousMatchButton");
+        setFixedSize(buttonPreviousMatch, buttonWidth, buttonHeight);
+        buttonPreviousMatch.addActionListener(actionListenerPreviousMatch);
+
+        buttonNextMatch = new JButton(new ImageIcon("\\icons\\arrowRight.png"));
+        buttonNextMatch.setName("NextMatchButton");
+        setFixedSize(buttonNextMatch, buttonWidth, buttonHeight);
+        buttonNextMatch.addActionListener(actionListenerNextMatch);
+
+
         northPanel.add(buttonOpen);
-        northPanel.add(Box.createHorizontalStrut(2));
+        northPanel.add(Box.createHorizontalStrut(widthBetweenComponents));
         northPanel.add(buttonSave);
+        northPanel.add(Box.createHorizontalStrut(widthBetweenComponents));
+        northPanel.add(textFieldFilepathAndSearch);
+        northPanel.add(Box.createHorizontalStrut(widthBetweenComponents));
+        northPanel.add(buttonStartSearch);
+        northPanel.add(Box.createHorizontalStrut(widthBetweenComponents));
+        northPanel.add(buttonPreviousMatch);
+        northPanel.add(Box.createHorizontalStrut(widthBetweenComponents));
+        northPanel.add(buttonNextMatch);
     }
 
     public static JComponent setFixedSize(JComponent component, int width, int height) {
